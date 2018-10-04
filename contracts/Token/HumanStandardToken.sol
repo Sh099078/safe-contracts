1f79ef5af5f4025655fe44c7507114db72ea7945
Loup Federico <loup.federico@yahoo.fr>
2018-10-04 17:22:38 +0200

    [CONTRACTS][TOKENS] Reimplement Token.sol, StandardToken.sol and HumanStandardToken.sol
      * Token.sol : The `Token` struct has become an interface since the methods it describes are
        implemented not directly in it but in the StandardToken contract.
      * StandardToken.sol : now reverts in case of integer overflow during the transactions.
      * HumanStandardToken.sol : Update the code. Public state variables getters are left
        for now so that the testsuite doesn't break but they should be removed eventually
        since public state variables getters are automatically generated at compile time.

--- contracts/Token/HumanStandardToken.sol
+++ contracts/Token/HumanStandardToken.sol
@@ -16,90 +16,70 @@ import "./StandardToken.sol";
 
 contract HumanStandardToken is StandardToken {
 
-    function () {
-        //if ether is sent to this address, send it back.
-        throw;
+    /// @dev Revert if the contract receives Ether.
+    function () external payable {
+        revert();
     }
 
     /* Public variables of the token */
 
     /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
+     * The following variables are OPTIONAL vanities. One does not have to include them.
+     * They allow to customise the token contract but do not influence the core functionality.
+     * Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
-    string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = 'H0.1';
     address public creator;
     bool alreadySetUp = false;
 
-    function HumanStandardToken ()
-    {
+    constructor() public {
         creator = msg.sender;
     }
 
-    function setUp(
-        uint256 _initialAmount,
-        string _tokenName,
-        uint8 _decimalUnits,
-        string _tokenSymbol
-        ) {
+    function setUp(uint256 _initialAmount, string _name, uint8 _decimals, string _symbol) public {
         require(msg.sender == creator);
         require(!alreadySetUp);
         balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
         totalSupply = _initialAmount;                        // Update total supply
-        name = _tokenName;                                   // Set the name for display purposes
-        decimals = _decimalUnits;                            // Amount of decimals for display purposes
-        symbol = _tokenSymbol;                               // Set the symbol for display purposes
-        alreadySetUp = true ;                                //Prevent it from being called a second time
+        name = _name;                                        // Set the name for display purposes
+        decimals = _decimals;                                // Amount of decimals for display purposes
+        symbol = _symbol;                                    // Set the symbol for display purposes
+        alreadySetUp = true ;                                // Prevent it from being called a second time
     }
 
-
     /* Approves and then calls the receiving contract */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
 
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
         //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+        if (!_spender.call(bytes4(bytes32(keccak256("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData))
+            revert();
         return true;
     }
 
-    function name()
-    public
-    view
-    returns (string _name)
-    {
+    // Getters. should be removed since they refer to public state variables,
+    // which getters are automatically generated at compile time.
+
+    function name() public view returns (string) {
         return name;
     }
 
-    function symbol()
-    public
-    view
-    returns (string _symbol)
-    {
+    function symbol() public view returns (string) {
         return symbol;
     }
 
-    function decimals()
-    public
-    view
-    returns (uint8 _decimals)
-    {
+    function decimals() public view returns (uint8) {
         return decimals;
     }
 
-    function creator()
-    public
-    view
-    returns (address _creator)
-    {
+    function creator() public view returns (address) {
         return creator;
     }
-
 }
--- contracts/Token/StandardToken.sol
+++ contracts/Token/StandardToken.sol
@@ -1,58 +1,75 @@
-/*
-This implements ONLY the standard functions and NOTHING else.
-For a token like you would want to deploy in something like Mist, see HumanStandardToken.sol.
-
-If you deploy this, you won't have anything useful.
-
-Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
-.*/
-
 pragma solidity ^0.4.4;
 
 import "./Token.sol";
 
+/// @dev This library protects contracts during of integer overflows during arithmetic operations.
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        require(a <= c, "Addition overflow");
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b <= a, "Substration overflow");
+        c = a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b, "Multiplication overflow");
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b != 0, "Division by 0");
+        c = a / b;
+    }
+}
+
+/// @dev Basic implementation of the ERC20 token standard : https://github.com/ethereum/EIPs/issues/20
+///      It shouldn't be deployed on its own but used to create more complex ERC20 tokens, such as
+///      the HumanStandardToken token.
 contract StandardToken is Token {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
+    using SafeMath for uint256;
 
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
     uint256 public totalSupply;
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        if (balances[msg.sender] < _value)
+            return false;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
+        if (allowed[_from][msg.sender] < _value || balances[_from] < _value)
+            return false;
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) external view returns (uint256 balance) {
+        balance = balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) external returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) external view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return totalSupply;
+    }
 }
